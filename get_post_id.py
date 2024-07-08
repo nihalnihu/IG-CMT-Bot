2180b305351c803653ab4e9962e6d1268c76fdc8
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-08 14:49:14 +0530

    Add files via upload

---
+++ get_post_id.py
@@ -0,0 +1,11 @@
+from instagrapi import Client
+from termcolor import colored
+getbot = Client()
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+getbot.login(username, password)
+
+post_url = input("Paste Instagram Post URL: ")
+postID = getbot.media_pk_from_url(post_url)
+print(colored(f"Post ID: {postID}", 'green'))
---
+++ send_comment.py
@@ -0,0 +1,20 @@
+from instagrapi import Client
+import time
+from termcolor import colored
+bot = Client()
+username = input(colored("Enter Your Instagram Username: ", 'blue' ))
+password = input(colored("Enter You Instagram Password: ", 'blue'))
+print("Trying... To Login")
+bot.login(username, password)
+print(f"{username} Login Success..")
+
+POSTID = input(colored("Enter Instagram Post ID: ", 'cyan'))
+commentmsg = input(colored("Enter Comment Message: ", 'yellow'))
+cmtcount = input(colored("How Meny Comment To Send: ", 'magenta'))
+i=1
+while i <= int(cmtcount):
+     bot.media_comment(POSTID, commentmsg)
+     print(colored(f"{username} Sended {i} Comment", 'green'))
+     time.sleep(3)
+     i+=1
+print(colored(f"Successfully Sended {cmtcount} Comments", 'green'))
