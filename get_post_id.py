58c71fd96e8f32fdf2485b945504d3e375e7a911
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-08 20:36:12 +0530

    Add files via upload

--- get_post_id.py
+++ get_post_id.py
@@ -1,12 +1,15 @@
 from instagrapi import Client
 from termcolor import colored
+import time
 
-getbot = Client()
-username = input("Enter Username: ")
-password = input("Enter Password: ")
+GetMediaID = Client()
 
-getbot.login(username, password)
+print("\nStarting...")
+time.sleep(3)
 
-post_url = input("Paste Instagram Post URL: ")
-postID = getbot.media_pk_from_url(post_url)
-print(colored(f"Post ID: {postID}", 'green'))
+post_url = input(colored("\nPaste Instagram Post URL: ", 'light_grey'))
+print(colored("\nGetting...", 'green'))
+time.sleep(2)
+postID = GetMediaID.media_pk_from_url(post_url)
+time.sleep(.5)
+print(colored(f"\nPost ID: {postID}\n", 'light_blue'))
