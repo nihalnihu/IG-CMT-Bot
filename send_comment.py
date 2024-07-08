feb9aaddcdd21c9f916580b40822270e6f422eaa
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-08 18:00:23 +0530

    Update send_comment.py

--- send_comment.py
+++ send_comment.py
@@ -1,16 +1,30 @@
+# Don't Forget To follow On Instagram @nihh____al 
+
 from instagrapi import Client
 import time
 from termcolor import colored
+
 bot = Client()
+
+#Enter Username and Password 
 username = input(colored("Enter Your Instagram Username: ", 'blue' ))
 password = input(colored("Enter You Instagram Password: ", 'blue'))
 print("Trying... To Login")
+
+#Login
 bot.login(username, password)
 print(f"{username} Login Success..")
 
+#Enter Post ID
 POSTID = input(colored("Enter Instagram Post ID: ", 'cyan'))
+
+#Enter Comment Message
 commentmsg = input(colored("Enter Comment Message: ", 'yellow'))
+
+#Enter How Meny Comments You Want To Send (int)
 cmtcount = input("How Meny Comment To Send: ")
+
+#loop Started
 i=1
 while i <= int(cmtcount):
      bot.media_comment(POSTID, commentmsg)
