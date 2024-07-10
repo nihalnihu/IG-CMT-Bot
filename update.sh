a422140ceff38ecf909ad73dba5788fa12dbefa2
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-11 00:50:39 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 green="\033[1;32m"
 white="\033[0m"
 prpl="\033[0;36m"
-echo -e ${green}"\n★ IG-CMT Updating..."
+echo -e ${green}"\n★ ɪɢ ᴄᴍᴛ ʙᴏᴛ Updating..."
 echo -e ${white}
 sleep 2
 git pull origin IGBot
