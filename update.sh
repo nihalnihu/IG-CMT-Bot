8203f0b2ee6d59588ceedb10dd0e4332cc84dcd8
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-11 13:13:45 +0530

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-green="\033[1;32m"
-white="\033[0m"
-prpl="\033[0;36m"
-echo -e ${green}"\n★ ɪɢ ᴄᴍᴛ ʙᴏᴛ Updating..."
-echo -e ${white}
-sleep 2
-git pull origin IGBot
-
-echo -e ${prpl}"\nᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ᴛʜɪs ᴛᴏᴏʟ.\n       sᴜᴘᴘᴏʀᴛ ᴜs\n"
