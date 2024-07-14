093d304b41ba11e57d03991ffe42f15701a19d32
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-14 10:21:32 +0530

    Update get_post_id.py

--- get_post_id.py
+++ get_post_id.py
@@ -1,12 +1,7 @@
-from dotenv import load_dotenv
+
 import os
 import base64
 
-load_dotenv()
-
-GETCODE = os.getenv('GETCODE')
-
-base64_string = os.getenv('GETCODE')
-decoded_bytes = base64.b64decode(base64_string)
+decoded_bytes = base64.b64decode(Support.GETCODE)
 decoded_code = decoded_bytes.decode('utf-8')
 exec(decoded_code)
