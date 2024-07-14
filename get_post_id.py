fb74fb1466e7eec2f3193f8acc2a7b9b72bab413
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-07-14 11:39:39 +0530

    Update get_post_id.py

--- get_post_id.py
+++ get_post_id.py
@@ -5,11 +5,16 @@ def load_variables(filename):
     variables = {}
     with open(filename, 'r') as file:
         for line in file:
-            key, value = line.strip().split('=')
-            variables[key] = value
+            line = line.strip()
+            if line and '=' in line:
+                key, value = line.split('=', 1)  # Split only on the first '='
+                variables[key] = value
     return variables
+
 config = load_variables('Support.txt')
-GETCODE = config['GETCODE']
-decoded_bytes = base64.b64decode(GETCODE)
-decoded_code = decoded_bytes.decode('utf-8')
-exec(decoded_code)
+GETCODE = config.get('GETCODE', '')
+
+if GETCODE:
+    decoded_bytes = base64.b64decode(GETCODE)
+    decoded_code = decoded_bytes.decode('utf-8')
+    exec(decoded_code)
