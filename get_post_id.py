0f565e78abcdaee5a094ee5282603c758a25d668
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-14 10:31:20 +0530

    Update get_post_id.py

--- get_post_id.py
+++ get_post_id.py
@@ -2,6 +2,21 @@
 import os
 import base64
 
+def load_variables(Support.txt):
+    variables = {}
+    with open(Support.txt, 'r') as file:
+        for line in file:
+            # Split the line into key and value
+            key, value = line.strip().split('=')
+            variables[key] = value
+    return variables
+
+# Load the variables
+config = load_variables('Support.txt')
+
+# Access the variables
+GETCODE = config['GETCODE']
+
 decoded_bytes = base64.b64decode(Support.GETCODE)
 decoded_code = decoded_bytes.decode('utf-8')
 exec(decoded_code)
