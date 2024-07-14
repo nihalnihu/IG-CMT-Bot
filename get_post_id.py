099371428f6d3a353421d47587bfc82140cce182
darkhacker34 <nihalthoppil16@gmail.com>
2024-07-14 09:53:01 +0530

    Add .env to .gitignore and remove from tracking

--- get_post_id.py
+++ get_post_id.py
@@ -1,5 +1,10 @@
+from dotenv import load_dotenv
 import os
 import base64
+
+load_dotenv()
+
+GETCODE = os.getenv('GETCODE')
 
 base64_string = os.getenv('GETCODE')
 decoded_bytes = base64.b64decode(base64_string)
