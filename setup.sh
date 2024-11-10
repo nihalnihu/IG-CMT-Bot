e029ae03d123691cd8696cc05eec807cfce00870
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-11-11 00:43:26 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Define color codes
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BRIGHT_BLACK='\033[1;90m'
+NC='\033[0m' # No color
+
+clear
+
+# Update package list
+echo -e "\n${YELLOW}Updating package list...${NC}"
+pkg update -y
+
+# Install required packages
+clear
+echo -e "\n${YELLOW}Installing git...${NC}"
+pkg install git -y
+
+clear
+echo -e "\n${YELLOW}Installing python...${NC}"
+pkg install python -y
+
+clear
+echo -e "\n${YELLOW}Installing libjpeg-turbo...${NC}"
+pkg install libjpeg-turbo -y
+
+# Install Python packages with pip
+
+clear
+echo -e "\n${YELLOW}Installing Libraries...${NC}"
+pip install Pillow --no-cache-dir
+pip install instagrapi==2.0.0
+pip install termcolor
+pip install colorama
+
+# Success message
+clear
+echo -e "\n\n${GREEN}All dependencies have been installed successfully!${NC}\n"
+
+# Final prompt to run get_post_id.py
+echo -e "${YELLOW}Now run ${BRIGHT_BLACK}python get_post_id.py${YELLOW}${NC}\n"
