f3d57ea0d2c381b3e08de0a5dce554a483c2a108
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-12-23 14:54:50 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,6 @@ no="\033[1;33m"
 echo -e ${no}"\n\tPlease Subscribe My YouTube Channel"
 sleep 3
 
-yt_url="https://youtube.com/@terminalbots"
-termux-open-url "$yt_url"
-
 green="\033[1;32m"
 white="\033[0m"
 prpl="\033[0;36m"
@@ -15,3 +12,6 @@ sleep 2
 git pull origin IGBot
 echo -e ${prpl}"\nᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ᴛʜɪs ᴛᴏᴏʟ.\n       sᴜᴘᴘᴏʀᴛ ᴜs\n"
 
+
+yt_url="https://youtube.com/@terminalbots"
+termux-open-url "$yt_url"
