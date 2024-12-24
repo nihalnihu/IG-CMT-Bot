60b7952cab79e2fc9a291852a9fe7d0532b8424f
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-12-24 15:32:58 +0530

    Update sc.py

--- sc.py
+++ sc.py
@@ -169,7 +169,6 @@ FIRST = """
 
 print_rounded_box(FIRST.format(username, POSTID, cmtcount))
 
-
 i = 1
 while i <= cmtcount:
     try:
@@ -177,7 +176,7 @@ while i <= cmtcount:
         bot.media_comment(POSTID, commentmsg)
         remaining = cmtcount - i
         sys.stdout.write("\033[K")
-        print(f"  Remaining: {remaining}  |  Success: {i} | Comment: {commentmsg}", end='\r', flush=True)
+        print(f"  Remaining: {remaining}  |  Success: {i}\nComment: {commentmsg}", end='\r', flush=True)
         time.sleep(deley)
         i += 1
     except Exception as e:
