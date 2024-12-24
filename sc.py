7bcc9d3d900976ca3bb61fb8fccd2aaac4e7dbdb
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-12-24 17:30:59 +0530

    Update sc.py

--- sc.py
+++ sc.py
@@ -181,7 +181,8 @@ while i <= cmtcount:
         remaining = cmtcount - i
         sys.stdout.write("\033[K")
         print(f"  Remaining: {remaining}  |  Success: {i} | Comment: {commentmsg}", end='\r', flush=True)
-        time.sleep(deley)
+        if remaining>0:
+          time.sleep(deley)
         i += 1
     except Exception as e:
         print(f"\n{Style.BRIGHT}{Fore.RED}Error while sending comment: {str(e)}")
